16bbfa6394a80d7f50bda8537d5233b7ebb20731
Dol174 <david.oleary174@gmail.com>
2016-11-04 16:03:26 +0000

    Initial commit, fixed enum bugs

---
+++ card_tests/CardTest.java
@@ -0,0 +1,42 @@
+import card_game.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CardTest {
+
+  Card twoOfClubs;
+
+  @Before
+  public void before(){
+    twoOfClubs = new Card(2, Suit.CLUBS);
+  }
+
+  @Test
+  public void hasNumber(){
+    assertEquals(2, twoOfClubs.getNumber());
+  }
+
+  @Test
+  public void hasSuit(){
+    assertEquals(Suit.CLUBS, twoOfClubs.getSuit());
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void numberNotBelowOne(){
+    Card card = new Card(0, Suit.SPADES);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void numberNotAbove13(){
+    Card card = new Card(14, Suit.SPADES);
+  }
+
+  @Test
+  public void canPrintString(){
+    Card aceOfSpades = new Card(1, Suit.SPADES);
+    Card kingOfClubs = new Card(13, Suit.CLUBS);
+    assertEquals("Ace of Spades", aceOfSpades.toString());
+    assertEquals("King of Clubs", kingOfClubs.toString());
+  }
+
+}
